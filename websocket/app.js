b8df4697f80046e1a8ba1ee4abea25906a6f2194
rjoeberttan <rosalesjoebert@gmail.com>
2021-11-26 06:45:16 +0800

    Websocket Change

--- websocket/app.js
+++ websocket/app.js
@@ -10,8 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: [process.env.FRONTEND],
-        methods: ["GET", "POST"]
+        origin: '*'
     },
 });
 
