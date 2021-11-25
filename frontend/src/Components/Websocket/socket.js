730694d32277f30d1e9b5ec20e573f53740cb319
rjoeberttan <rosalesjoebert@gmail.com>
2021-11-26 07:09:45 +0800

    fe websocket change

--- frontend/src/Components/Websocket/socket.js
+++ frontend/src/Components/Websocket/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const socket = io.connect(process.env.REACT_APP_HEADER_WEBSOCKET);
+const socket = io.connect("http://localhost:3010");
 
 
 export default socket;
