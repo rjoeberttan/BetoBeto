48823868e3c9d33205d02d8d1b321be5387317d8
rjoeberttan <rosalesjoebert@gmail.com>
2022-04-12 17:59:10 +0800

    Sakla backend choices and create new game

--- backend_sakla/app.js
+++ backend_sakla/app.js
@@ -132,6 +132,124 @@ async function updateMarketStatus(marketId, gameId, description, status, editor,
   })
 }
 
+async function getSaklaChoices(gameId) {
+  return new Promise((resolve, reject) => {
+    sqlQuery = "SELECT * FROM choices WHERE game_id = ?"
+    db_pool.query(sqlQuery, [gameId], (err, result) => {
+        if (err) {
+          logger.error(`Error in getting sakla choices gameId:${gameId}, error:${err}`)
+          return (reject(err.message))
+        } else {
+          logger.info(`Retrieved sakla choices for gameId:${gameId}`)
+          return (resolve(result))
+        }
+    })
+  })
+}
+
+async function updateChoiceDetail(choiceId, description, manipulateValue, editor){
+  return new Promise((resolve, reject) => {
+    sqlQuery = "UPDATE choices SET description = ?, manipulatse_val = ? , lastedit_date = NOW(), lastedit_by = ? WHERE choice_id = ?"
+    db_pool.query(sqlQuery, [description, manipulateValue, editor, choiceId], (err, result) => {
+        if (err) {
+          logger.error(`Error in updating sakla choice choiceId:${choiceId}, error:${err}`)
+          return (reject(err.message))
+        } else {
+          logger.info(`Successfully updated sakla choice details, choiceId:${choiceId} description:${description} manipulateValue:${manipulateValue}`)
+          return (resolve(result))
+        }
+    })
+  })
+}
+
+async function updateSaklaWinMultiplier(gameId, multiplier, editor) { 
+  return new Promise((resolve, reject) => {
+    sqlQuery = "UPDATE games SET win_multip1 = ?, lastedit_date = NOW(), edited_by = ? WHERE game_id = ?"
+    db_pool.query(sqlQuery, [multiplier, editor, gameId], (err, result) => {
+        if (err) {
+          logger.error(`Error in updating sakla choice choiceId:${choiceId}, error:${err}`)
+          return (reject(err.message))
+        } else {
+          logger.info(`Successfully updated sakla win multiplier, gameId:${gameId} winMultiplier:${multiplier}`)
+          return (resolve(result))
+        }
+    })
+  })
+}
+
+async function createNewSaklaGame(gameName, editor) { 
+  return new Promise((resolve, reject) => {
+
+    const name = gameName
+    const basename = "Sakla"
+    const banner = `Welcome to ${name}. Enjoy Playing`
+    const minBet = parseFloat(500)
+    const maxBet = parseFloat(1000)
+    const youtubeUrl = "0i7eX_45JWQ"
+    const winMutlip = 18
+    const commission = 10
+    const type = 3
+
+    sqlQuery = "INSERT INTO games (name, basename, banner, min_bet, max_bet, youtube_url, win_multip1, created_date, lastedit_date, edited_by, commission, type) \
+    VALUES \
+    (?, ?, ?, ?, ?, ?, ?, NOW(), NOW(), ?, ?, ?)"
+    db_pool.query(sqlQuery, [name, basename, banner,minBet, maxBet, youtubeUrl, winMutlip,  editor, commission, type], (err, result) => {
+        if (err) {
+          logger.error(`Error in creating new Sakla Game gameName:${name}, error:${err}`)
+          return (reject(err.message))
+        } else {
+          logger.info(`Successfully created New Sakla Game, gameId:${result.insertId}`)
+          return (resolve(result))
+        }
+    })
+  })
+}
+
+async function insertNewChoices(gameId, editor) { 
+  return new Promise((resolve, reject) => {
+
+    const choices = ['1 ESPADA - 2 ESPADA',
+        '3 ESPADA - 4 ESPADA',
+        '5 ESPADA - 6 ESPADA',
+        '7 ESPADA - SOTANG ESPADA',
+        'KABAYONG ESPADA - HARING ESPADA',
+        '1 OROS - 2 OROS',
+        '3 OROS - 4 OROS',
+        '5 OROS - 6 OROS',
+        '7 OROS - SOTANG OROS',
+        'KABAYONG OROS - HARING OROS',
+        '1 KOPAS - 2 KOPAS',
+        '3 KOPAS - 4 KOPAS',
+        '5 KOPAS - 6 KOPAS',
+        '7 KOPAS - SOTANG KOPAS',
+        'KABAYONG KOPAS - HARING KOPAS',
+        '1 BASTOS - 2 BASTOS',
+        '3 BASTOS - 4 BASTOS',
+        '5 BASTOS - 6 BASTOS',
+        '7 BASTOS - SOTANG BASTOS',
+        'KABAYONG BASTOS - HARING BASTOS'
+    ]
+
+    sqlQuery = "INSERT INTO choices (description, game_id, manipulate_val, lastedit_by) VALUES "
+    choices.forEach((choice) => {
+      sqlQuery += `(\'${choice}\', ${gameId}, 0.0, \'${editor}\'), `
+    })
+    sqlQuery = sqlQuery.slice(0, -2) + ';'
+    db_pool.query(sqlQuery, (err, result) => {
+      if (err) {
+          logger.error(`Error in inserting default choices for game:${gameId}, error:${err}`)
+          return (reject(err.message))
+        } else {
+          logger.info(`Successfully inserted default choices for sakla game, gameId:${gameId}`)
+          return (resolve(result))
+        }
+    })
+  })
+}
+
+
+
+
 app.post("/createMarket", async (req, res) => {
   const startTime = process.hrtime();
 
@@ -192,6 +310,9 @@ app.post("/createMarket", async (req, res) => {
   }
 });
 
+
+
+
 app.post("/openMarket", async(req, res) => {
   const startTime = process.hrtime();
 
@@ -221,6 +342,7 @@ app.post("/openMarket", async(req, res) => {
   const market =  await getMarketStatus(marketId, gameId)
     .catch((error) => {
         res.status(500).json({message: "Server Error"})
+        return
     })
   
   // If market is not found
@@ -241,6 +363,7 @@ app.post("/openMarket", async(req, res) => {
   const result = updateMarketStatus(marketId, gameId, market[0].description, 0, editor, null)
     .catch((error) => {
       res.status(500).json({message: "Server Error"})
+      return
     })
   res.status(200).json({message: "Market opened successfully", data: {marketId: marketId, status: 0}})
 
@@ -276,6 +399,7 @@ app.post("/closeMarket", async(req, res) => {
   const market =  await getMarketStatus(marketId, gameId)
     .catch((error) => {
         res.status(500).json({message: "Server Error"})
+        return
     })
   
   // If market is not found
@@ -296,6 +420,7 @@ app.post("/closeMarket", async(req, res) => {
   const result = updateMarketStatus(marketId, gameId, market[0].description, 1, editor, null)
     .catch((error) => {
       res.status(500).json({message: "Server Error"})
+      return
     })
   res.status(200).json({message: "Market closed successfully", data: {marketId: marketId, status: 1}})
 
@@ -332,6 +457,7 @@ app.post("/resultMarket", async(req, res) => {
   const market =  await getMarketStatus(marketId, gameId)
     .catch((error) => {
         res.status(500).json({message: "Server Error"})
+        return
     })
   
   // If market is not found
@@ -352,9 +478,164 @@ app.post("/resultMarket", async(req, res) => {
   const result = updateMarketStatus(marketId, gameId, market[0].description, 2, editor, marketResult)
     .catch((error) => {
       res.status(500).json({message: "Server Error"})
+      return
     })
   res.status(200).json({message: "Market settled successfully", data: {marketId: marketId, status: 2}})
 
+})
+
+
+app.get("/getChoices", async(req, res) => {
+  const startTime = process.hrtime();
+
+  // Headers and Data
+  const apiKey = req.header("Authorization");
+  const gameId = req.body.gameId;
+
+  // Check if apiKey is correct
+  if (!checkApiKey(apiKey)) {
+    logger.warn(`${req.originalUrl} request has missing/wrong apiKey, received ${apiKey}, body: ${JSON.stringify(req.body)}`);
+    res.status(401).json({ message: "Unauthorized Request" });
+    return
+  }
+
+  // Check if Body is complete
+  if (!gameId ) {
+    logger.warn(logger.warn(`${req.originalUrl} request has missing parameters, body: ${JSON.stringify(req.body)}`));
+    res.status(422).json({ message: "Request has missing parameters"});
+    return
+  }
+
+  // Get Choices for GameId Selected
+  logger.info(`${req.originalUrl} started ${JSON.stringify(req.body)}`)
+
+  const choices = await getSaklaChoices(gameId)
+    .catch((error) => {
+      res.status(500).json({message: "Server Error"})
+      return
+    })
+  
+  res.status(200).json({message: "Choices fetched successfully", data: {choices: choices}})
+  return
+})
+
+
+app.patch("/updateChoiceDetails", async (req, res) => {
+  const startTime = process.hrtime();
+
+  // Headers and Data
+  const apiKey = req.header("Authorization");
+  const gameId = req.body.gameId
+  const choiceId = req.body.choiceId
+  const description = req.body.description
+  const manipulateValue = req.body.manipulateValue
+  const editor = req.body.editor
+
+  // Check if apiKey is correct
+  if (!checkApiKey(apiKey)) {
+    logger.warn(`${req.originalUrl} request has missing/wrong apiKey, received ${apiKey}, body: ${JSON.stringify(req.body)}`);
+    res.status(401).json({ message: "Unauthorized Request" });
+    return
+  }
+
+  // Check if Body is complete
+  if (!gameId || !choiceId || !description || !editor || !manipulateValue) {
+    logger.warn(logger.warn(`${req.originalUrl} request has missing parameters, body: ${JSON.stringify(req.body)}`));
+    res.status(422).json({ message: "Request has missing parameters"});
+    return
+  }
+
+  // Update choices 
+  logger.info(`${req.originalUrl} started ${JSON.stringify(req.body)}`)
+
+  const result = await updateChoiceDetail(choiceId, description, manipulateValue, editor)
+    .catch((error) => {
+      res.status(500).json({message: "Server Error"})
+      return
+    })
+  
+  res.status(200).json({message: "Choice successfully edited", data: {choiceId: choiceId, description: description, manipulateValue: manipulateValue}})
+  return
+})
+
+app.patch("/updateSaklaWinMultiplier", async (req, res) => {
+  const startTime = process.hrtime();
+
+  // Headers and Data
+  const apiKey = req.header("Authorization");
+  const gameId = req.body.gameId
+  const multiplier = req.body.multiplier
+  const editor = req.body.editor
+
+  // Check if apiKey is correct
+  if (!checkApiKey(apiKey)) {
+    logger.warn(`${req.originalUrl} request has missing/wrong apiKey, received ${apiKey}, body: ${JSON.stringify(req.body)}`);
+    res.status(401).json({ message: "Unauthorized Request" });
+    return
+  }
+
+  // Check if Body is complete
+  if (!gameId ||  !editor || !multiplier) {
+    logger.warn(logger.warn(`${req.originalUrl} request has missing parameters, body: ${JSON.stringify(req.body)}`));
+    res.status(422).json({ message: "Request has missing parameters"});
+    return
+  }
+
+  // Update choices 
+  logger.info(`${req.originalUrl} started ${JSON.stringify(req.body)}`)
+
+  const result = await updateSaklaWinMultiplier(gameId, multiplier, editor)
+    .catch((error) => {
+      res.status(500).json({message: "Server Error"})
+      return
+    })
+  
+  res.status(200).json({message: "Win Multiplier edited successfully ", data: {gameId: gameId, winMultiplier: multiplier}})
+  return
+})
+
+
+app.post("/createNewSaklaGame", async (req, res) => {
+  const startTime = process.hrtime();
+
+  // Headers and Data
+  const apiKey = req.header("Authorization");
+  const gameName = req.body.gameName;
+  const editor = req.body.editor
+
+  // Check if apiKey is correct
+  if (!checkApiKey(apiKey)) {
+    logger.warn(`${req.originalUrl} request has missing/wrong apiKey, received ${apiKey}, body: ${JSON.stringify(req.body)}`);
+    res.status(401).json({ message: "Unauthorized Request" });
+    return
+  }
+
+  // Check if Body is complete
+  if (!gameName || !editor) {
+    logger.warn(logger.warn(`${req.originalUrl} request has missing parameters, body: ${JSON.stringify(req.body)}`));
+    res.status(422).json({ message: "Request has missing parameters"});
+    return
+  }
+
+  // Insert New Games 
+  logger.info(`${req.originalUrl} started ${JSON.stringify(req.body)}`)
+  const result = await createNewSaklaGame(gameName, editor)
+    .catch((error) => {
+      res.status(500).json({message: "Server Error"})
+      return
+  })
+
+  // Insert Choices
+  const newGameId = result.insertId
+  const choicesResult = await insertNewChoices(newGameId, editor)
+  .catch((error) => {
+    res.status(500).json({message: "Server Error"})
+    return
+  })
+
+  logger.info(`Successfully created new sakla game and choices. gameName:${gameName} gameId:${result.insertId} duration:${getDurationInMilliseconds(startTime)}`)
+  res.status(200).json({message: "Created New Sakla Game Successfully", data: {gameId: result.insertId}})
+  return
 })
 
 
